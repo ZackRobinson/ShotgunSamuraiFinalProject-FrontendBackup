cd02aad70410609ae2b55e8f7457a38242107775
Edel <edelalexanderfb@gmail.com>
2017-10-16 21:35:21 -0400

    Button Functionalities Added           - Submit           - Cancel

--- app/src/main/java/com/zackeryrobinson/finalprojecttest/MainActivity.java
+++ app/src/main/java/com/zackeryrobinson/finalprojecttest/MainActivity.java
@@ -1,19 +1,29 @@
 package com.zackeryrobinson.finalprojecttest;
 
 import android.app.AlertDialog;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.net.ParseException;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements PayFragment.OnFragmentInteractionListener, BenefitsFragment.OnFragmentInteractionListener, TrainingFragment.OnFragmentInteractionListener, MarketingFragment.OnFragmentInteractionListener{
+import java.util.Date;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements PayFragment.OnFragmentInteractionListener, BenefitsFragment.OnFragmentInteractionListener, TrainingFragment.OnFragmentInteractionListener, MarketingFragment.OnFragmentInteractionListener {
+
+    private static final String TAG = "MainActivityTag";
 
     private AccountFragment frag;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -24,24 +34,23 @@ public class MainActivity extends AppCompatActivity implements PayFragment.OnFra
             switch (item.getItemId()) {
                 case R.id.navigation_account:
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new AccountFragment(), "frag").commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new AccountFragment(), "frag").commit();
                     return true;
                 case R.id.navigation_pay:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new PayFragment(), "frag").commit();
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new PayFragment(), "frag").commit();
 
 
                     return true;
                 case R.id.navigation_benefits:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new BenefitsFragment(), "frag").commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new BenefitsFragment(), "frag").commit();
 
                     return true;
                 case R.id.navigation_training:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new TrainingFragment(), "frag").commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new TrainingFragment(), "frag").commit();
 
                     return true;
                 case R.id.navigation_marketing:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new MarketingFragment(), "frag").commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content, new MarketingFragment(), "frag").commit();
 
                     return true;
             }
@@ -67,24 +76,43 @@ public class MainActivity extends AppCompatActivity implements PayFragment.OnFra
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-
-
     }
 
     EditText tvRequestPto_dateFrom;
     EditText tvRequestPto_dateTo;
+    EditText tvRequestPto_description;
+    List<RequestedPtoClass> requestedPtos;
     private AlertDialog dialog;
-    public void requestPto(View view) {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void requestPto(View view) {  ////////////////////////////////////////////onClick Method of Request PTO Button
         final AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
 
         View mView = getLayoutInflater().inflate(R.layout.alert_dialog_req_pto, null);
 
         tvRequestPto_dateFrom = mView.findViewById(R.id.tvRequestPto_dateFrom);
         tvRequestPto_dateTo = mView.findViewById(R.id.tvRequestPto_dateTo);
+        tvRequestPto_description = mView.findViewById(R.id.tvRequestPto_description);
 
         mBuilder.setView(mView);
         dialog = mBuilder.create();
         dialog.show();
+
+        /////////////////////////////////////////////////////////Submit Button Functionality
+        {
+            DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+
+            try {
+                requestedPtos.add(new RequestedPtoClass((df.parse(tvRequestPto_dateFrom.getText().toString())),
+                        df.parse(tvRequestPto_dateTo.getText().toString()), tvRequestPto_description.getText().toString()));
+            } catch (java.text.ParseException e) {
+                e.printStackTrace();
+                Log.d(TAG, "requestPto: " + e);
+            }
+        }
+        {////////////////////////////////////////////////////////Cancel Button Functionality
+            dialog.dismiss();
+        }
     }
 
 
